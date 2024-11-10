122236f132e3d336c211bba7e2287f09966bd9df
yaow <1753552156@qq.com>
2024-11-10 20:41:14 +0800

    feat: 完成拖拽逻辑

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,3 +1,64 @@
+import React, { useEffect } from "react";
+import { useComponentConfigStore } from "../stores/component-config";
+import { Component, useComponetsStore } from "../stores/components";
+
 export function EditArea() {
-  return <div>EditArea</div>;
+  const { components } = useComponetsStore();
+  const { componentConfig } = useComponentConfigStore();
+
+  // useEffect(() => {
+  //   addComponent(
+  //     {
+  //       id: 222,
+  //       name: "Container",
+  //       props: {},
+  //       children: [],
+  //     },
+  //     1
+  //   );
+
+  //   addComponent(
+  //     {
+  //       id: 333,
+  //       name: "Button",
+  //       props: {
+  //         text: "无敌",
+  //       },
+  //       children: [],
+  //     },
+  //     222
+  //   );
+  // }, []);
+
+  function renderComponents(components: Component[]): React.ReactNode {
+    console.log("components", components);
+
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+
+      if (!config?.component) {
+        return null;
+      }
+
+      return React.createElement(
+        config.component,
+        {
+          key: component.id,
+          id: component.id,
+          name: component.name,
+          ...config.defaultProps,
+          ...component.props,
+        },
+        renderComponents(component.children || [])
+      );
+    });
+  }
+
+  return (
+    //  className="h-[100%]"
+    <div className="h-[100%]">
+      {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
+      {renderComponents(components)}
+    </div>
+  );
 }
---
+++ src/editor/materials/Page/index.tsx
@@ -0,0 +1,20 @@
+import { CommonComponentProps } from "../../interface";
+import { useMaterailDrop } from "../../hook/useMaterialDrop";
+
+function Page({ id, name, children }: CommonComponentProps) {
+  console.log(id, name, "id,name");
+
+  const { canDrop, drop } = useMaterailDrop(["Button", "Container"], id);
+
+  return (
+    <div
+      ref={drop}
+      className="p-[20px] h-[100%] box-border"
+      style={{ border: canDrop ? "2px solid blue" : "none" }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Page;
