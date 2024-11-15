eeedd60352490f43a5a5ac66eaa6f2e5ce417580
yaow <1753552156@qq.com>
2024-11-15 21:29:29 +0800

    feat: add 事件弹窗

--- src/editor/stores/component-config.tsx
+++ src/editor/stores/component-config.tsx
@@ -12,6 +12,10 @@ export interface ComponentSetter {
   type: string;
   [key: string]: any;
 }
+export interface ComponentEvent {
+  name: string;
+  label: string;
+}
 
 export interface ComponentConfig {
   name: string;
@@ -21,6 +25,7 @@ export interface ComponentConfig {
   stylesSetter?: ComponentSetter[];
   dev: any;
   prod: any;
+  events?: ComponentEvent[];
 }
 
 interface State {
@@ -72,6 +77,16 @@ export const useComponentConfigStore = create<State & Action>((set) => ({
           name: "height",
           label: "高度",
           type: "inputNumber",
+        },
+      ],
+      events: [
+        {
+          name: "onClick",
+          label: "点击事件",
+        },
+        {
+          name: "onDoubleClick",
+          label: "双击事件",
         },
       ],
       desc: "按钮",
